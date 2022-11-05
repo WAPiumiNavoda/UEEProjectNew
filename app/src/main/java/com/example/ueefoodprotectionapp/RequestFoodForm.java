c81c8128bc3d505daabfc4e42a3fbfaca7e00983
Piumi Navoda <it20617028@my.sliit.lk>
2022-11-05 14:08:01 +0530

    add request form

---
+++ app/src/main/java/com/example/ueefoodprotectionapp/RequestFoodForm.java
@@ -0,0 +1,14 @@
+package com.example.ueefoodprotectionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestFoodForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_food_form);
+    }
+}
